bce6df0bbb23037380af129fc920e06d7c82cfb9
Marisa Paolucci <marisapaolucciart@gmail.com>
2024-07-29 15:03:02 -0400

    code and comments added

--- list2.py
+++ list2.py
@@ -6,8 +6,20 @@
 # modify the passed in list.
 
 def remove_adjacent(nums):
-    # +++your code here+++
-    return
+    if not nums:
+        return []
+    new_list = [nums[0]]
+    for num in nums[1:]:
+        if num != new_list[-1]:
+            new_list.append(num)
+    return new_list
+
+# defined function remove_adjacent and passing parameter "nums" through it.
+# second line checks to see if the list is empty.
+# created variable list1. [nums[0]] makes sure that the first element in the list is always included.
+# the for loop iterates over the elements of 'nums' starting from the second element [1:]. This creates a sublist that excludes the first number.
+# the if statement checks to see if the current element 'num' is different from the last element in the new_list list. 'new_list[-1]' refers to the last element in the new_list list. 
+# new_list.append(num) appends the current element 'num' to the 'new_list' list if it is different from the last result in new_list.
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -16,9 +28,33 @@ def remove_adjacent(nums):
 # pass of both lists.
 
 def linear_merge(list1, list2):
-    # +++your code here+++
-    return
+    a, b = 0, 0
+# a, b are used as pointers for list1 and list2 as empty lists to store the merged results
+    merged_list = []
 
+    while a < len(list1) and b < len(list2):
+# creates a loop to go through the entire list.
+        if list1[a] < list2[b]:
+# compares the two lists. If list1[a] is smaller, it appends list1[a] to merged_list and increments 'a'.
+            merged_list.append(list1[a])
+            a += 1
+        else: 
+            merged_list.append(list2[b])
+            b += 1
+# otherwise it appends list2[b] to merged_list and increments b.
+
+    while a < len(list1):
+        merged_list.append(list1[a])
+        a += 1
+# in case there are remaining elements in list1, this while loop will append the elements to the merged_list
+
+    while b < len(list2):
+        merged_list.append(list2[b])
+        b += 1
+# in case there are remaining elements in list2, this while loop will append the elements to the merged_list
+    return merged_list
+
+# I originally tried a simple sorted function. However, I learned that this is not efficient for linear time. The two-pointer approach is more efficient and can be used for larger lists.
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
